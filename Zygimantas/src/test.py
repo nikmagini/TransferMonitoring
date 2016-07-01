c9a7af64b31ca7d0fe9c00f1322f26199b7fb39d
Zygimantas Matonis <zmatonis@lxplus065.cern.ch>
2016-07-01 10:41:26 +0200

    small changes

--- Zygimantas/src/test.py
+++ Zygimantas/src/test.py
@@ -1,2 +1,11 @@
-print("jahja=aaa")
+import sys
 
+
+def printSomething():
+    print("akuna matata")
+
+print("-------------------------------")
+printSomething()
+
+a = sys.stdin.readline()
+print("as sakiau: " + a)
