05ebb8a065c84ebfc99e3dad0590786c7bfadc29
zygis <mr.matonis@gmail.com>
2016-07-22 17:00:38 +0200

    small changes

--- Zygimantas/src/Python/checkPredictions.py
+++ Zygimantas/src/Python/checkPredictions.py
@@ -12,14 +12,15 @@ import os
 import sys
 import getopt  # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 import itertools
+import numpy
 
 
 def main(argv):
     """Main function"""
     # todo implement random split of dataset
-    predictFile = '/tmp/pred.txt'
+    predictFile = '/tmp/predic.txt'
     realFiles = '/tmp/test.csv'
-    resultsFile = '/tmp/results.txt'
+    resultsFile = '/tmp/check_results.txt'
 
     label = 'timestamp_tr_dlt'
 
@@ -48,21 +49,33 @@ def main(argv):
             #     print a
 
             predicted_val.readline()
-            header = "Real_value, Predict, absolute_diference\n"
+            header = "Real_value,Predict,Absolute_diference"
+            header += ",Diference_in_percentages\n"
             results_wr.write(header)
             count = 0
             total_offset = 0
+            sq_offset = 0
             for f, b in itertools.izip(real_Readercsv, predicted_val):
                 # TODO: try to catch -1
-                String = "{0}, {1}, {2}\n".format(
-                    f[label], b.rstrip(), + abs(float(f[label]) - float(b)))
+
+                real_val = float(f[label])
+                pred_val = float(b)
+                abs_diff = abs(real_val - pred_val)
+                dif_in_p = (pred_val * 100) / real_val
+                String = "{0},{1},{2},{3:.2f}\n".format(
+                    real_val, pred_val, abs_diff, dif_in_p)
                 results_wr.write(String)
                 count += 1
                 total_offset += abs(float(f[label]) - float(b))
+                sq_offset += abs(float(f[label]) - float(b))**2
+                print sq_offset
             MSE = total_offset / count
+            RMSE = numpy.sqrt(sq_offset / count)
             String = "----------------\n"
-            String += "MAE: {0}".format(MSE)
-            results_wr.write(String)
+            String += "MAE: {0}\n".format(MSE)
+            String += "RMSE: {0}\n".format(RMSE)
+            print(String)
+
     except Exception, e:
         raise
 
