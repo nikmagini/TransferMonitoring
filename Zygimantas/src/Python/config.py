9c12bf486cac84134b90d3510977328897778dfa
zygis <mr.matonis@gmail.com>
2016-08-04 11:57:21 +0200

    json utilities.py: string are converted to float using log(), strings are checked if they represent float, added config.py if I would like to store conf files otuside

---
+++ Zygimantas/src/Python/config.py
@@ -0,0 +1,15 @@
+# columns that are output and cant be used with ML
+drop_out_colums = ['timestamp_tr_comp',
+                   'timestamp_chk_src_ended',
+                   'timestamp_checksum_dest_ended',
+                   'timestamp_checksum_dest_ended',
+                   'tr_error_scope',
+                   't_failure_phase',
+                   'tr_error_category',
+                   't_final_transfer_state',
+                   'tr_bt_transfered',
+                   'time_srm_prep_end',
+                   'time_srm_fin_end',
+                   't__error_message',
+                   'tr_timestamp_complete'
+                   ]
--- Zygimantas/src/Python/jsonUtilities.py
+++ Zygimantas/src/Python/jsonUtilities.py
@@ -12,8 +12,10 @@ import sys
 import getopt  # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 import hashlib
 import logging
-import inspect
 import types
+import math
+
+from config import drop_out_colums as drop_fields
 
 # global variables
 field_delimter = "|"  # to seperate flatened fields
@@ -21,34 +23,49 @@ cvs_field_ph = -1    # placeholder for empty atribute
 cvs_field_sph = -2    # placeeholder for blanklines
 cvs_field_nph = -3    # placeeholder for null
 # list of fields that I should put first in CSV file
-important_fields = ['tr_id', 'timestamp_tr_st',
-                    'timestamp_tr_comp', 'timestamp_tr_dlt']
-new_fields =['timestamp_tr_dlt']
+important_fields = ['tr_id', 'timestamp_tr_st']
+new_fields = ['timestamp_tr_dlt']
+
 
 # logger and logger config
 # https://docs.python.org/2/library/logging.html
 FORMAT = '%(levelname)s - %(funcName)s - %(lineno)d: %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
+# logger.setLevel("DEBUG")
 
 # files:
 working_path = os.path.dirname(os.path.abspath(__file__))
 
+inputfile = os.path.join(
+    working_path, "../../../data/20160629_fts_message_sample.json")
+# inputfile = os.path.join(working_path, "../../../data/big.json")
 # inputfile = os.path.join(working_path, "../../data/testFolder")
 # inputfile = os.path.join(working_path, "../../data/smallJsonData.json")
-inputfile = None
+# inputfile = None
 
 # outputfile_org = '/tmp/JsonToCsvDefault_org.csv'
 outputfile_org = None
 
 # outputfile_hash = '/tmp/JsonToCsvDefault_hash.csv'
-outputfile_hash = None
+outputfile_hash = os.path.join(
+    working_path, "../../data/output/json_hashed.csv")
+# outputfile_hash = None
 
 
 def representsInt(s):
     """check if string can be typecasted to int"""
     try:
         int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def representsFloat(s):
+    """check if string can be typecasted to float"""
+    try:
+        float(s)
         return True
     except ValueError:
         return False
@@ -67,11 +84,8 @@ def flatten_dict(d):
     return dict(items())
 
 
-def reorderKeyList(key_list, import_k_l,new_fields):
+def reorderKeyList(key_list, import_k_l):
     """put important keys in front of list"""
-
-    # and new fields to list
-    key_list = key_list + new_fields
 
     # reoder keys
     for key in import_k_l:
@@ -96,6 +110,7 @@ def readFolderToListGenerator(FolderPath):
                 for json_dict in readFileToListGenerator(a):
                     yield json_dict
 
+
 def readFileToListGenerator(filename):
     """
     :param filename: path to file
@@ -111,14 +126,18 @@ def readFileToListGenerator(filename):
     escapes = ''.join([chr(char) for char in range(1, 32)])
     with open(filename) as json_file:
         for line in json_file:
-            json_dict = flatten_dict(json.loads(line.translate(None, escapes)))
-            logger.debug(json_dict)
-            yield json_dict
+            try:
+                json_dict = flatten_dict(
+                    json.loads(line.translate(None, escapes)))
+                yield json_dict
+            except Exception as e:
+                logger.exception(e)
 
 
 def stringToHash(string):
     """it will take atribute, probably string and return a big integer"""
-    return int(hashlib.md5(string).hexdigest()[:30], 16)
+    # return int(hashlib.md5(string).hexdigest()[:16], 16)
+    return math.log(int(hashlib.md5(string).hexdigest(), 16))
 
 
 def recordsListTransform(records_dict):
@@ -132,6 +151,8 @@ def recordsListTransform(records_dict):
         if isinstance(value, basestring):
             if(representsInt(value)):
                 records_dict[key] = int(value)
+            elif(representsFloat(value)):
+                records_dict[key] = float(value)
             elif(value.upper() == 'TRUE'):
                 records_dict[key] = 1
             elif(value.upper() == 'FALSE'):
@@ -149,25 +170,30 @@ def recordsListTransform(records_dict):
     return records_dict
 
 
-def jsonListToCSV(jsonList, keysSet, outputPath, hash_f=False ):
+def jsonListToCSV(jsonList, keysSet, outputPath, hash_f=False):
     """
     Function that processes list and output results to Csv file
     """
 
     outputhFile = open(outputPath, 'w')
     csvwriter = csv.DictWriter(
-        outputhFile, fieldnames=keysSet, restval=cvs_field_ph)
+        outputhFile, fieldnames=keysSet, restval=cvs_field_ph,
+        extrasaction='ignore')
     csvwriter.writeheader()
-    logger.debug('File writing')
-    for record_dict in jsonList:
-        record_dict = addDeltaTimeField(record_dict)
-        if hash_f is True: # to hash values
-            record_dict = recordsListTransform(record_dict)
-
-        logger.debug(record_dict)
-        csvwriter.writerow(record_dict)
-    outputhFile.close()
+    logger.debug('Writing File')
+    try:
+        for record_dict in jsonList:
+            record_dict = addDeltaTimeField(record_dict)
+            if hash_f is True:  # to hash values
+                record_dict = recordsListTransform(record_dict)
+            csvwriter.writerow(record_dict)
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        outputhFile.close()
+        logger.info('Closing output file')
     return 0
+
 
 def addDeltaTimeField(jsonDict):
     """
@@ -186,14 +212,11 @@ def addDeltaTimeField(jsonDict):
         jsonDict['timestamp_tr_dlt'] = cvs_field_ph
     return jsonDict
 
+
 def keys_from_gen(gen_function):
     '''
     :param gen_function: takes generator and extracts unique dictonary field names
     :return: set()
-    '''
-    '''
-    :param gen_function:
-    :return:
     '''
     key_set = set()
     for item in gen_function:
@@ -210,22 +233,22 @@ def main(argv):
     """Main function"""
     global inputfile,  outputfile_org, outputfile_hash, working_path
 
-    message= 'Convertes .json file/folder with .json to .csv\n' \
-             'To run :\n' \
-             'jsonUtilities.py --ifile <inputfile.json|dir> --ofile <outputfile.csv> \n\n' \
-             '--ohfile <output_hashedfile.csv> --log <LEVEL>\n\n' \
-             'example: \n' \
-             'jsonUtilities.py --ifile ../../data/testFolder --ohfile ' \
-             '/tmp/output_hashed.csv --log WARNING \n'\
-             '-h : shows this help \n' \
-             '-i/--ifile : input file or a directory \n' \
-             '-o/--ofile : path/to/extracted/output.csv \n'\
-             '--ohfile : path/to/hashed/output.csv \n' \
-             '--log : message level [DEBUG|INFO|WARNING|ERROR|CRITICAL] \n'
+    message = 'Convertes .json file/folder with .json to .csv\n' \
+        'To run :\n' \
+        'jsonUtilities.py --ifile <inputfile.json|dir> --ofile <outputfile.csv> \n\n' \
+        '--ohfile <output_hashedfile.csv> --log <LEVEL>\n\n' \
+        'example: \n' \
+        'jsonUtilities.py --ifile ../../data/testFolder --ohfile ' \
+        '/tmp/output_hashed.csv --log WARNING \n'\
+        '-h : shows this help \n' \
+        '-i/--ifile : input file or a directory \n' \
+        '-o/--ofile : path/to/extracted/output.csv \n'\
+        '--ohfile : path/to/hashed/output.csv \n' \
+        '--log : message level [DEBUG|INFO|WARNING|ERROR|CRITICAL] \n'
 
     try:
         opts, args = getopt.getopt(
-            argv, "hi:o:", ["ifile=", "ofile=", "ohfile=","log="])
+            argv, "hi:o:", ["ifile=", "ofile=", "ohfile=", "log="])
     except getopt.GetoptError:
         print('ERROR:Wrong arguments'
               'run \'jsonUtilities.py -h\' for more info')
@@ -245,13 +268,12 @@ def main(argv):
             try:
                 logger.setLevel(arg.upper())
             except Exception as e:
-                logger.warning('%s, using INFO logging level ',e)
+                logger.warning('%s, using INFO logging level ', e)
 
     if not (inputfile and (outputfile_hash or outputfile_org)):
         print('Atleast input and one output parameter should be given, '
               'run \'jsonUtilities.py -h\' for more info')
         sys.exit(2)
-
 
     # end of user input
     # ---------------
@@ -265,7 +287,7 @@ def main(argv):
             logger.info('Input file is: %s', inputfile)
         else:
             logger.warning('Input direction/file '
-                             'does not exist: %s', inputfile)
+                           'does not exist: %s', inputfile)
             sys.exit(2)
     except Exception as e:
         logger.exception(e)
@@ -275,23 +297,29 @@ def main(argv):
     # end of file checking
     # ---------------
 
-    # take all unique keys from list and put to set and order it
+    # Take all unique keys from list and put to set and order it.
+    # Later it will be used as a header for .csv file
     records_keys = keys_from_gen(records_list_generator(inputfile))
-
+    # remove unavailable keys
+    records_keys = records_keys.difference(drop_fields)
     # sort list keys alphabetically
     records_keys = sorted(records_keys)
+    # change intresting fields position
+    records_keys = reorderKeyList(records_keys, important_fields)
+    # add new fields to the header to the end of file
+    records_keys = records_keys + new_fields
 
-    # add new fiels and change few fields position
-    records_keys = reorderKeyList(records_keys, important_fields,new_fields)
     logger.debug('Sorted key list : %s', records_keys)
 
     # write every thing to CSV file
     if outputfile_org:
-        jsonListToCSV(records_list_generator(inputfile), records_keys, outputfile_org, hash_f=False)
+        jsonListToCSV(records_list_generator(inputfile),
+                      records_keys, outputfile_org, hash_f=False)
 
     # write every thing to CSV file with Hashed values
     if outputfile_hash:
-        jsonListToCSV(records_list_generator(inputfile), records_keys, outputfile_hash, hash_f=True )
+        jsonListToCSV(records_list_generator(inputfile),
+                      records_keys, outputfile_hash, hash_f=True)
 
 
 if __name__ == '__main__':
